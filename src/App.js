a06a8341c0afba143cafd18b685a6e1e7bb92f9c
Guy <guykibby@gmail.com>
2023-09-15 12:02:09 +1000

    Add skeleton react app
    Co-Authored-By: C. Scooter Alexander <scooterseoul@gmail.com>
    Co-Authored-By: kays <52937130+shvinprog@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+// Components for each route
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Rooms from "./components/Rooms";
+import Categories from "./components/Categories";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        {/* You can add a Navbar here if needed */}
+
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/:room" element={<Rooms />} />
+        <Route path="/categories/:catagory" element={<Categories />} />
+
+        {/* You can add a 404 Not Found route here if needed */}
+        {/* <Route component={NotFound} /> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
