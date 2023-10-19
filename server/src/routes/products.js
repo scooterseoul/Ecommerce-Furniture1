4e50476a1c41146bafeea11b5c14a4a6aa6f392a
Guy <guykibby@gmail.com>
2023-10-20 10:02:21 +1100

    Update products.js
    Co-Authored-By: C. Scooter Alexander <scooterseoul@gmail.com>
    Co-Authored-By: kays <52937130+shvinprog@users.noreply.github.com>

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -16,6 +16,53 @@ router.get("/", async (req, res, next) => {
         return { ...product, price };
       })
     );
+    const chairs = [];
+    const tables = [];
+    const sofas = [];
+    const accessories = [];
+
+    const livingroom = [];
+    const dining = [];
+    const outdoors = [];
+    const office = [];
+
+    const otherTypes = [];
+    const otherRooms = [];
+
+    for (let i = 0; i < products.length; i++) {
+      if (products[i].metadata.type === "chair") {
+        chairs.push(products[i]);
+      } else if (products[i].metadata.type === "table") {
+        tables.push(products[i]);
+      } else if (products[i].metadata.type === "sofa") {
+      } else if (products[i].metadata.type === "accessory") {
+        accessories.push(products[i]);
+      } else {
+        otherTypes.push(products[i]);
+      }
+      if (products[i].metadata.room === "livingroom") {
+        livingroom.push(products[i]);
+      } else if (products[i].metadata.room === "dining") {
+        dining.push(products[i]);
+      } else if (products[i].metadata.room === "outdoors") {
+        outdoors.push(products[i]);
+      } else if (products[i].metadata.room === "office") {
+        office.push(products[i]);
+      } else {
+        otherRooms.push(products[i]);
+      }
+    }
+    console.log(products.length);
+    console.log("chairs", chairs.length);
+    console.log("tables", tables.length);
+    console.log("sofas", sofas.length);
+    console.log("accessories", accessories.length);
+    console.log("otherTypes", otherTypes.length);
+    console.log("livingroom", livingroom.length);
+    console.log("dining", dining.length);
+    console.log("outdoors", outdoors.length);
+    console.log("office", office.length);
+    console.log("otherRooms", otherRooms.length);
 
     res.status(200).json(productsWithPrices);
   } catch (error) {
