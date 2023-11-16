32b48c1ed729f56692f188bd99b60f1d4f8c8b38
Claudette Alexander <claudette.alexander@developersinstitute.co.nz>
2023-11-16 09:56:23 +0900

    Success.js edits
    Co-Authored-By: Guy <guykibby@gmail.com>
    Co-Authored-By: kays <52937130+shvinprog@users.noreply.github.com>

--- client/src/components/Success.js
+++ client/src/components/Success.js
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import styles from "./Success.module.css";
 const Success = () => {
-  return <div>Success</div>;
+  return (
+    <div className={styles.successCont}>
+      <Link to="/" className={styles.backToShopping}>
+        <h1>&#127881;</h1>
+        Success!
+        <p> &#8592; Back to Shopping</p>
+      </Link>
+    </div>
+  );
 };
 
 export default Success;
