480eedd7e30286dbbd96050b170f2f95d95d2a02
Guy <guykibby@gmail.com>
2023-11-24 10:27:42 +1100

    Update products.js
    Co-Authored-By: C. Scooter Alexander <scooterseoul@gmail.com>
    Co-Authored-By: kays <52937130+shvinprog@users.noreply.github.com>

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -22,55 +22,6 @@ router.get("/", async (req, res, next) => {
       };
     });
 
-    const chairs = [];
-    const tables = [];
-    const sofas = [];
-    const accessories = [];
-
-    const livingroom = [];
-    const dining = [];
-    const outdoors = [];
-    const office = [];
-
-    const otherTypes = [];
-    const otherRooms = [];
-
-    for (let i = 0; i < products.length; i++) {
-      if (products[i].metadata.type === "chair") {
-        chairs.push(products[i]);
-      } else if (products[i].metadata.type === "table") {
-        tables.push(products[i]);
-      } else if (products[i].metadata.type === "sofa") {
-        sofas.push(products[i]);
-      } else if (products[i].metadata.type === "accessory") {
-        accessories.push(products[i]);
-      } else {
-        otherTypes.push(products[i]);
-      }
-      if (products[i].metadata.room === "livingroom") {
-        livingroom.push(products[i]);
-      } else if (products[i].metadata.room === "dining") {
-        dining.push(products[i]);
-      } else if (products[i].metadata.room === "outdoors") {
-        outdoors.push(products[i]);
-      } else if (products[i].metadata.room === "office") {
-        office.push(products[i]);
-      } else {
-        otherRooms.push(products[i]);
-      }
-    }
-    console.log(products.length);
-    console.log("chairs", chairs.length);
-    console.log("tables", tables.length);
-    console.log("sofas", sofas.length);
-    console.log("accessories", accessories.length);
-    console.log("otherTypes", otherTypes.length);
-    console.log("livingroom", livingroom.length);
-    console.log("dining", dining.length);
-    console.log("outdoors", outdoors.length);
-    console.log("office", office.length);
-    console.log("otherRooms", otherRooms.length);
-
     res.status(200).json(productsWithPrices);
   } catch (error) {
     next(error);
@@ -79,14 +30,28 @@ router.get("/", async (req, res, next) => {
 
 router.post("/create-checkout-session", async (req, res) => {
   const lineItems = req.body.line_items;
-  // console.log("SERVER : " + JSON.stringify(lineItems));
-  const session = await Stripe.checkout.sessions.create({
-    line_items: lineItems,
-    mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel",
-  });
-  res.json({ url: session.url });
+
+  const CURRENT_DOMAIN = "http://localhost:5001/api/products";
+  try {
+    const session = await Stripe.checkout.sessions.create({
+      line_items: lineItems,
+      mode: "payment",
+      success_url: `${CURRENT_DOMAIN}/success`,
+      cancel_url: `${CURRENT_DOMAIN}/cancel`,
+    });
+    res.json({ url: session.url });
+  } catch (error) {
+    res.json({ url: `${CURRENT_DOMAIN}/cancel` });
+    console.log({ "Error : ": error.message });
+  }
+});
+
+router.get("/cancel", (req, res) => {
+  res.redirect("http://localhost:3000/cart?cancelled=true");
+});
+
+router.get("/success", async (req, res) => {
+  res.redirect("http://localhost:3000/cart?successful=true");
 });
 
 module.exports = router;
